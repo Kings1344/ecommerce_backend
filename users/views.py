c5763afaa364b7aebca714c84116b1e3ed4ddeb0
USER PC <uchennachimaobi9@gmail.com>
2026-07-12 04:18:56 -0700

    Added API home page

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from .serializers import RegisterSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class RegisterView(APIView):
@@ -17,7 +17,7 @@ class RegisterView(APIView):
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 class ProfileView(APIView):
-        permission_classes = [IsAuthenticated]
+        permission_classes = [AllowAny]
 
         def get(self, request):
             return Response({
